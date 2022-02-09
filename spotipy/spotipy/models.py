c69479c7c010c179923a04e56bc225e7538f7589
shirzlotnik <shirzlotnik@gmail.com>
2022-02-09 08:57:31 +0200

    track, album, artist classes

---
+++ spotipy/spotipy/models.py
@@ -0,0 +1,17 @@
+class Track:
+    def __init__(self, id: str, name: str, popularity: int):
+        self.id = id
+        self.name = name
+        self.popularity = popularity
+
+
+class Album:
+    def __init__(self, id: str, name: str):
+        self.id = id
+        self.name = name
+
+
+class Artist:
+    def __init__(self, id: str, name: str):
+        self.id = id
+        self.name = name
