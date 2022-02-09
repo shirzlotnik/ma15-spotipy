d535fcd9210e7772451623b7162bb0e5be2e53eb
shirzlotnik <shirzlotnik@gmail.com>
2022-02-09 11:39:20 +0200

    added playlist validation for user

--- spotipy/users/users.py
+++ spotipy/users/users.py
@@ -14,20 +14,29 @@ class User:
         self._playlist_names = []
         self.premium = premium
 
+    def playlist_validation_for_user(self, playlist: Playlist):
+        if not self.premium:
+            if len(self.playlist) > 5:
+                print(f'you have reached the maximum amount of playlist, to add more consider upgrading to premium')
+                return False
+
+            if len(playlist.tracks) > 20:
+                print(f'a playlist can have up to 20 tracks, to add more consider upgrading to premium')
+                return False
+
+        return True
+
     def add_playlist_decorator(self, add_playlist):
         def inner(playlist: Playlist):
-            if not self.premium:
-                if len(self.playlist) > 5:
-                    print(f'you have reached the maximum amount of playlist, to add more consider upgrading to premium')
-                    return
-
-            if len(self.playlist) == 0:
-                add_playlist(playlist)
-            else:
-                if playlist.name in self._playlist_names:
-                    print(f'playlist {playlist.name} already exist')
-                else:
+            if self.playlist_validation_for_user(playlist):
+                if len(self.playlist) == 0:
                     add_playlist(playlist)
+                    
+                else:
+                    if playlist.name in self._playlist_names:
+                        print(f'playlist {playlist.name} already exist')
+                    else:
+                        add_playlist(playlist)
 
         return inner
 
@@ -36,3 +45,4 @@ class User:
         self.playlist.append(playlist)
         self._playlist_names.append(playlist.name)
         print(f'playlist {playlist.name} added successfully')
+
