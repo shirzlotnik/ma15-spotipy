4235493711493058c1b77885058456a12d7ae3bb
shirzlotnik <shirzlotnik@gmail.com>
2022-02-09 09:51:29 +0200

    added add_parameter function to classes

--- spotipy/spotipy/models.py
+++ spotipy/spotipy/models.py
@@ -1,20 +1,36 @@
 from typing import List
 
 
-class Track:
-    def __init__(self, id: str, name: str, popularity: int):
+class Artist:
+    def __init__(self, id: str, name: str):
         self.id = id
         self.name = name
-        self.popularity = popularity
+        self.albums = []
+
+    def add_album(self, album):
+        self.albums.append(album)
 
 
 class Album:
     def __init__(self, id: str, name: str):
         self.id = id
         self.name = name
+        self.tracks = []
+
+    def add_track(self, track):
+        self.tracks.append(track)
 
 
-class Artist:
-    def __init__(self, id: str, name: str):
+class Track:
+    def __init__(self, id: str, name: str, popularity: int):
         self.id = id
         self.name = name
+        self.popularity = popularity
+        self.album = None
+        self.artists = []
+
+    def add_album(self, album: Album):
+        self.album = album
+
+    def add_artists(self, artists: List[Artist]):
+        self.artists = artists
